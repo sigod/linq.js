a1e6b57d50eda7fea18dd5bb0cb245fa331e8fb6
Michael Novikov <sigod.mail@gmail.com>
2012-06-28 23:12:18 +0300

    Added first code. Added functions: where and toArray.

---
+++ linq.js
@@ -0,0 +1,41 @@
+
+var LINQ = function (source, operations) {
+	this._source = source;
+	this._operations = operations || [];
+};
+
+LINQ.prototype = {
+	/// Filters a sequence of values based on a predicate. Each element's index is used in the logic of the predicate function.
+	/// predicate<element, int, boolean>
+	where: function (predicate) {
+		var operations = [];
+
+		this._operations.forEach(function (operation) {
+			operations.push(operation);
+		});
+
+		operations.push({
+			name: 'where',
+			predicate: predicate
+		});
+
+		return new LINQ(this._source, operations);
+	},
+	/// Creates an array.
+	toArray: function () {
+		var self = this;
+
+		var array = [];
+
+		// perform operations
+		this._operations.forEach(function (operation) {
+			if (operation.name === 'where') {
+				array = self._source.filter(function (element, index) {
+					return operation.predicate(element, index);
+				});
+			}
+		});
+
+		return array;
+	}
+};
